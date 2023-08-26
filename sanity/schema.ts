bc90112e48eaac377608c96dc3e261ba40be0f57
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2023-08-25 23:25:27 -0400

    Add FAQs Sanity IO Schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity';
 import organization from './schemas/organization';
 import rental from './schemas/rental';
+import faqs from './schemas/faqs';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [organization, rental],
+  types: [organization, rental, faqs],
 };
---
+++ sanity/schemas/faqs.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'faqs',
+  title: 'FAQs',
+  type: 'document',
+  fields: [
+    {
+      name: 'question',
+      title: 'Question',
+      type: 'string',
+    },
+    {
+      name: 'answer',
+      title: 'Answer',
+      type: 'text',
+    },
+  ],
+};
